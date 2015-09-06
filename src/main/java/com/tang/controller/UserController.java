798e7542fcd1aae4c62583567521c186797a9d06
Jerry <tangthis@163.com>
2015-09-06 16:15:52 +0800

    增加上传文件功能

--- src/main/java/com/tang/controller/UserController.java
+++ src/main/java/com/tang/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.tang.controller;
 
+import java.io.File;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.tang.po.TestUser;
 import com.tang.service.UserService;
@@ -40,5 +45,29 @@ public class UserController {
 		TestUser data = userService.get(id);
 		Result ret = new Result(ResultStatus.OK.value(), data);
 		return ret;
+	}
+	/**
+	 * 上传图像
+	 * @param file
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value="/icon")
+	@ResponseBody
+	public Result icon(@RequestParam(value = "file", required = true) MultipartFile file,HttpServletRequest request){
+		String path = request.getSession().getServletContext().getRealPath("upload");  
+        String fileName = file.getOriginalFilename();  
+        File targetFile = new File(path, fileName);  
+        if(!targetFile.exists()){  
+            targetFile.mkdirs();  
+        }
+  
+        try {  
+            file.transferTo(targetFile);
+            return new Result(ResultStatus.OK.value(),"上传成功");
+        } catch (Exception e) {  
+            e.printStackTrace();
+            return new Result(ResultStatus.INTERNAL_SERVER_ERROR.value(),"系统异常");
+        }
 	}
 }
